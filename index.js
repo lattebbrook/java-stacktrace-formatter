9a47ee99fa50a95a8359fbf45d4fd901aff60555
tanawat.brook <tanawat.brook@icloud.com>
2026-07-25 20:38:36 +0700

    feat: add index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Route root to your specific HTML file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'stacktrace-formatter.html')); 
+});
+
+// Serve the rest of your static files normally
+app.use(express.static(__dirname));
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
